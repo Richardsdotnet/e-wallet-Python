2bbb20259a10483ade0f23860a0c405e23863a6d
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-08-21 14:23:52 +0100

    updated

--- ewalletApp/views.py
+++ ewalletApp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+
+from ewalletApp.models import Wallet, Transaction, User
+from ewalletApp.serializers import WalletSerializer, TransactionSerializer, UserSerializer
+
+
+class WalletViewSet(ModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
+
+
+class TransactionViewSet(ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
